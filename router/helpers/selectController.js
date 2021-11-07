420b01226d4988b944066becbec59d2e7661e0cb
Filip Biterski <filip.biterski@gmail.com>
2021-11-07 23:25:59 +0100

    Update router

--- router/helpers/selectController.js
+++ router/helpers/selectController.js
@@ -1,12 +1,11 @@
 module.exports = function buildSelectController(controllers) {
   return function selectController(pathSegments) {
     let selectedController = controllers;
-    let pathParams = [];
     for (let i = 0; i < pathSegments.length; i++) {
       const segment = pathSegments[i];
       if (selectedController.controllers) {
         if (selectedController.subParameter) {
-          req.pathParameters.push({[selectedController.subParameter]: segment});
+          req.pathParameters[selectedController.subParameter] = segment;
           selectedController =
             selectedController.controllers[selectedController.subParameter];
         } else {
