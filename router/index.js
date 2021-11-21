9e1e86efb41c6f0c1cd488426c40134308b6751a
Filip Biterski <filip.biterski@gmail.com>
2021-11-21 23:11:16 +0100

    Fix server error handling and response

--- router/index.js
+++ router/index.js
@@ -6,13 +6,13 @@ module.exports = function buildRouter(controllers) {
     const selectController = buildSelectController(controllers)
     const selectMethod = buildSelectMethod(selectController)
 
-    return function router(req, res) {
+    return async function router(req, res) {
         const selectedMethod = selectMethod(req)
         if (!selectedMethod) {
             res.statusCode = 404
             res.end()
             return
         }
-        selectedMethod(req, res)
+        await selectedMethod(req, res)
     }
 }
--- server/helpers/root-request-handler.js
+++ server/helpers/root-request-handler.js
@@ -1,14 +1,14 @@
 module.exports = function buildRootRequestHandler(router, makeRequestObject, makeResponseObject) {
-    return function rootRequestHandler(nodeRequestObject, nodeResponseObject) {
+    return async function rootRequestHandler(nodeRequestObject, nodeResponseObject) {
         const requestObject = makeRequestObject(nodeRequestObject)
         const responseObject = makeResponseObject(nodeResponseObject)
         try {
             console.log(`${requestObject.methodName.toUpperCase()} ${requestObject.url}`)
-            router(requestObject, responseObject)
+            await router(requestObject, responseObject)
         } catch(exception) {
             console.warn('Root Request Handler caught an excepetion:')
             console.error(exception)
-            responseObject.flushHeaders()
+            responseObject.removeHeader('Content-Type')
             responseObject.statusCode = 500
             responseObject.end()
             console.warn('Root Request Handler sent status code 500.')
