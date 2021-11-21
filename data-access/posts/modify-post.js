1b4263bd97e2dca343b09cc282310b2b62688475
Filip Biterski <filip.biterski@gmail.com>
2021-11-21 20:49:03 +0100

    Fix file system flag for post modification

--- data-access/posts/modify-post.js
+++ data-access/posts/modify-post.js
@@ -6,7 +6,7 @@ module.exports = function buildModifyPost({fs, path, dirPath, makeDataResult}) {
         const postString = JSON.stringify(post)
 
         const modifyResult = await fs
-            .writeFile(filePath, postString, {flag: 'r+'})
+            .writeFile(filePath, postString, {flag: 'w'})
             .then(() => [null, post])
             .catch(error => [error.code == 'ENOENT' ? null : error, null])
             .then(result => makeDataResult(result))
